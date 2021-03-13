542efeba845bbe4513ce191f144da0dd17f3ce91
AnthonyArmour <anthonydarmour@gmail.com>
2021-03-13 01:05:01 +0000

    created outline for _printf function and added to .h

--- _printf.c
+++ _printf.c
@@ -1,1 +1,49 @@
 #include "holberton.h"
+/**
+ * _printf - produces an output according to a format
+ * @format: str input
+ * Return: num of characters printed excluding \0
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int x = 0, y = 0;
+	int count = 0;
+
+	c_spec specs[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_d},
+		{'i', print_i},
+		{'\0', NULL}
+	};
+	va_start(list, format);
+	while (format && format[x])
+	{
+		if (format[x] == '%')
+		{
+			y = 0;
+			if (format[x + 1] == '%')
+			{
+				_putchar(format[x + 1]);
+				count++;
+			}
+			while (specs[y].letter)
+			{
+				if (specs[y].letter == format[x + 1])
+				{
+					specs[y].f(count, list);
+				}
+				y++;
+			}
+
+		}
+		else
+		{
+			_putchar(format[x]);
+			count++;
+		}
+		x++;
+	}
+	return (count);
+}
--- holberton.h
+++ holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+/* STRUCTS */
+typedef struct conversion_specifier{
+	char letter;
+	int (*f)(int count, va_list list);
+}c_spec;
 /* INCLUDES */
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +12,6 @@
 #include <unistd.h>
 
 /* FUNCTION PROTOTYPES */
-int _putchar(char c)
-
+int _putchar(char c);
+int _printf(const char *format, ...);
 #endif
